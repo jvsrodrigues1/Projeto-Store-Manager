f52f4fd12d6a44c92ed2a5c8f16ca770e068d9bb
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-02-27 11:17:45 -0300

    Ajustes app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const productRouter = require('./routes/product.routes');
 
 const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productRouter);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
