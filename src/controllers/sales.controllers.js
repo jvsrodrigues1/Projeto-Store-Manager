dad1ce11d81e53d0df69b8b7b679c71bd71d5737
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-02-27 11:39:04 -0300

    Sales service finalizado, sales controllers iniciado

---
+++ src/controllers/sales.controllers.js
@@ -0,0 +1,19 @@
+const salesService = require('../services/sales.services');
+const errorMap = require('../utilities/errorMap');
+
+const getSales = async (_req, res) => {
+  const { type, message } = await salesService.getSales();
+
+  if (type) return res.status(type).json(message);
+
+  return res.status(200).json(message);
+};
+
+const getSaleById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await salesService.getSalesById(id);
+
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+
+  res.status(200).json(message);
+};
